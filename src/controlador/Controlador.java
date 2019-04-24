1b28401686f8f46d3cc236d9b1be1f5f238587cf
Daniel Castillo <casvilldaniel@gmail.com>
2019-04-24 15:50:25 -0500

    Ya se pueden añadir productos a la tabla de productos desde la pesataña segunda. (Falta hacer que no se repitan los códigos. Se está haciendo uso de ArrayList para guardar los productos, aún falta hacer la base de datos real).

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -2,28 +2,93 @@ package controlador;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;  //borrar
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
 import modelo.Producto;
 import vista.Vista;
 
 public class Controlador implements ActionListener {
-
+    
     private Vista vista;
     private Producto producto;
-
-    public Controlador(Vista vista, Producto producto) {
+    private DefaultTableModel modelo;
+    
+    private ArrayList<Producto> productos; //borrar
+    
+    
+    
+    //Constructor
+    public Controlador(Vista vista) 
+    {   
         this.vista = vista;
-        this.producto = producto;
+        this.vista.setVisible(true);
         
-        this.vista.jtBusquedaPorCodigo.addActionListener(this);
+        this.vista.jbAñadir.addActionListener(this);
+        this.vista.jtfBusquedaPorCodigo.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                jtBusquedaPorCodigoKeyPressed(evt);
+            }
+        });
+        
+        productos = new ArrayList<>(); //borrar
+        
+        //Modelo de tabla
+        modelo = (DefaultTableModel) vista.jtProductos.getModel();
+       //Fin modelo de tabla------
     }
     
+    //Manejador de eventos al presionar un botón en el campo de texto para la búsqueda por código de producto:
+    private void jtBusquedaPorCodigoKeyPressed(java.awt.event.KeyEvent evt) {                                               
+        
+    }     
     
     
+    //Manejador de eventos al presionar un botón:
     @Override
-    public void actionPerformed(ActionEvent e) {
-
+    public void actionPerformed(ActionEvent e) 
+    {
+        if(e.getSource() == vista.jbAñadir)
+        {
+            Producto p = new Producto();
+            p.setCodigoProducto(vista.jtfCodigoProducto.getText().trim());
+            p.setDescripcion(vista.jtfDescripcion.getText().trim());
+            p.setGrado(vista.jtfGrado.getText().trim());
+            try
+            {
+                p.setExistenciasIniciales(Integer.parseInt(vista.jtfExistenciasIniciales.getText().trim()));
+                p.setCostoUnitario(Float.parseFloat(vista.jtfCostoUnitario.getText().trim()));
+                productos.add(p);//borrar
+                actualizarTablaProductos();
+                JOptionPane.showMessageDialog(null, "Producto añadido exitosamente.");
+            }catch(NumberFormatException error)
+            {
+                JOptionPane.showMessageDialog(null, "Error. El campo de existencias iniciales y costo unitario deben ser valores numéricos.");
+            }
+            
+        }
+    }
+    
+    public void actualizarTablaProductos()
+    {
+        Object []object = new Object[5];
+        object[0] = vista.jtfCodigoProducto.getText();
+        object[1] = vista.jtfDescripcion.getText();
+        object[2] = vista.jtfGrado.getText();
+        object[3] = vista.jtfExistenciasIniciales.getText(); 
+        object[4] = vista.jtfCostoUnitario.getText();
+        
+        vista.jtfCodigoProducto.setText("");
+        vista.jtfDescripcion.setText("");
+        vista.jtfGrado.setText("");        
+        vista.jtfExistenciasIniciales.setText("");        
+        vista.jtfCostoUnitario.setText("");
+                
+        modelo.addRow(object);   
     }
     
     
-    
+    public static void main(String args[]) {
+        Controlador control = new Controlador(new Vista());
+    }
 }
--- src/vista/Vista.java
+++ src/vista/Vista.java
@@ -2,6 +2,7 @@ package vista;
 
 public class Vista extends javax.swing.JFrame {
 
+    
     public Vista() {
         initComponents();
     }
@@ -13,14 +14,28 @@ public class Vista extends javax.swing.JFrame {
         jTabbedPane1 = new javax.swing.JTabbedPane();
         jPanel1 = new javax.swing.JPanel();
         jScrollPane1 = new javax.swing.JScrollPane();
-        jtableGeneral = new javax.swing.JTable();
+        jtProductos = new javax.swing.JTable();
         jlBusquedaPorCodigo = new javax.swing.JLabel();
-        jtBusquedaPorCodigo = new javax.swing.JTextField();
+        jtfBusquedaPorCodigo = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
+        jlCodigoProducto = new javax.swing.JLabel();
+        jtfCodigoProducto = new javax.swing.JTextField();
+        jlDescripcion = new javax.swing.JLabel();
+        jtfDescripcion = new javax.swing.JTextField();
+        jlGrado = new javax.swing.JLabel();
+        jtfGrado = new javax.swing.JTextField();
+        jlExistenciasIniciales = new javax.swing.JLabel();
+        jtfExistenciasIniciales = new javax.swing.JTextField();
+        jlCostoUnitario = new javax.swing.JLabel();
+        jtfCostoUnitario = new javax.swing.JTextField();
+        jbAñadir = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jtableGeneral.setModel(new javax.swing.table.DefaultTableModel(
+        jTabbedPane1.setFont(new java.awt.Font("Arial", 0, 12)); // NOI18N
+
+        jtProductos.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
 
             },
@@ -43,20 +58,20 @@ public class Vista extends javax.swing.JFrame {
                 return canEdit [columnIndex];
             }
         });
-        jScrollPane1.setViewportView(jtableGeneral);
-        if (jtableGeneral.getColumnModel().getColumnCount() > 0) {
-            jtableGeneral.getColumnModel().getColumn(0).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(0).setPreferredWidth(110);
-            jtableGeneral.getColumnModel().getColumn(1).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(2).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(3).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(4).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(4).setPreferredWidth(40);
-            jtableGeneral.getColumnModel().getColumn(5).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(5).setPreferredWidth(40);
-            jtableGeneral.getColumnModel().getColumn(6).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(7).setResizable(false);
-            jtableGeneral.getColumnModel().getColumn(7).setPreferredWidth(30);
+        jScrollPane1.setViewportView(jtProductos);
+        if (jtProductos.getColumnModel().getColumnCount() > 0) {
+            jtProductos.getColumnModel().getColumn(0).setResizable(false);
+            jtProductos.getColumnModel().getColumn(0).setPreferredWidth(110);
+            jtProductos.getColumnModel().getColumn(1).setResizable(false);
+            jtProductos.getColumnModel().getColumn(2).setResizable(false);
+            jtProductos.getColumnModel().getColumn(3).setResizable(false);
+            jtProductos.getColumnModel().getColumn(4).setResizable(false);
+            jtProductos.getColumnModel().getColumn(4).setPreferredWidth(40);
+            jtProductos.getColumnModel().getColumn(5).setResizable(false);
+            jtProductos.getColumnModel().getColumn(5).setPreferredWidth(40);
+            jtProductos.getColumnModel().getColumn(6).setResizable(false);
+            jtProductos.getColumnModel().getColumn(7).setResizable(false);
+            jtProductos.getColumnModel().getColumn(7).setPreferredWidth(30);
         }
 
         jlBusquedaPorCodigo.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
@@ -69,21 +84,21 @@ public class Vista extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 755, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 804, Short.MAX_VALUE)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jlBusquedaPorCodigo)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                        .addComponent(jtBusquedaPorCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addComponent(jtfBusquedaPorCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, 142, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addGap(0, 0, Short.MAX_VALUE)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addContainerGap(130, Short.MAX_VALUE)
+                .addContainerGap(253, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jlBusquedaPorCodigo)
-                    .addComponent(jtBusquedaPorCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(jtfBusquedaPorCodigo, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addGap(40, 40, 40)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 310, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
@@ -91,18 +106,92 @@ public class Vista extends javax.swing.JFrame {
 
         jTabbedPane1.addTab("General", jPanel1);
 
+        jlCodigoProducto.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jlCodigoProducto.setText("Código del producto:");
+
+        jlDescripcion.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jlDescripcion.setText("Descripción:");
+
+        jlGrado.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jlGrado.setText("Grado:");
+
+        jlExistenciasIniciales.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jlExistenciasIniciales.setText("Existencias Iniciales:");
+
+        jlCostoUnitario.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+        jlCostoUnitario.setText("Costo Unitario:");
+
+        jbAñadir.setText("Añadir");
+
+        jLabel1.setIcon(new javax.swing.ImageIcon("C:\\Users\\Magneto\\Documents\\NetBeansProjects\\CuttingBlades\\src\\vista\\valsider.jpg")); // NOI18N
+
         javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
         jPanel2.setLayout(jPanel2Layout);
         jPanel2Layout.setHorizontalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 775, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(302, 302, 302)
+                        .addComponent(jLabel1))
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGap(225, 225, 225)
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jlCodigoProducto)
+                                .addGap(29, 29, 29)
+                                .addComponent(jtfCodigoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addComponent(jlDescripcion)
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jtfDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE))
+                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jlGrado)
+                                    .addComponent(jlExistenciasIniciales)
+                                    .addComponent(jlCostoUnitario))
+                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(jtfExistenciasIniciales, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jtfGrado, javax.swing.GroupLayout.PREFERRED_SIZE, 181, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jtfCostoUnitario, javax.swing.GroupLayout.PREFERRED_SIZE, 89, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                    .addComponent(jbAñadir, javax.swing.GroupLayout.PREFERRED_SIZE, 180, javax.swing.GroupLayout.PREFERRED_SIZE))))))
+                .addContainerGap(259, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 511, Short.MAX_VALUE)
+            .addGroup(jPanel2Layout.createSequentialGroup()
+                .addGap(44, 44, 44)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addGroup(jPanel2Layout.createSequentialGroup()
+                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addGroup(jPanel2Layout.createSequentialGroup()
+                                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                    .addGroup(jPanel2Layout.createSequentialGroup()
+                                        .addComponent(jLabel1)
+                                        .addGap(66, 66, 66)
+                                        .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                                            .addComponent(jlCodigoProducto)
+                                            .addComponent(jtfCodigoProducto, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                        .addGap(18, 18, 18)
+                                        .addComponent(jlDescripcion))
+                                    .addComponent(jtfDescripcion, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGap(18, 18, 18)
+                                .addComponent(jlGrado))
+                            .addComponent(jtfGrado, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addGap(18, 18, 18)
+                        .addComponent(jlExistenciasIniciales))
+                    .addComponent(jtfExistenciasIniciales, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jlCostoUnitario)
+                    .addComponent(jtfCostoUnitario, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30)
+                .addComponent(jbAñadir, javax.swing.GroupLayout.PREFERRED_SIZE, 33, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(102, Short.MAX_VALUE))
         );
 
-        jTabbedPane1.addTab("tab2", jPanel2);
+        jTabbedPane1.addTab("Añadir Nuevo Producto", jPanel2);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -118,17 +207,27 @@ public class Vista extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    public static void main(String args[]) {
-
-    }
+    
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTabbedPane jTabbedPane1;
+    public javax.swing.JButton jbAñadir;
     private javax.swing.JLabel jlBusquedaPorCodigo;
-    public javax.swing.JTextField jtBusquedaPorCodigo;
-    public javax.swing.JTable jtableGeneral;
+    private javax.swing.JLabel jlCodigoProducto;
+    private javax.swing.JLabel jlCostoUnitario;
+    private javax.swing.JLabel jlDescripcion;
+    private javax.swing.JLabel jlExistenciasIniciales;
+    private javax.swing.JLabel jlGrado;
+    public javax.swing.JTable jtProductos;
+    public javax.swing.JTextField jtfBusquedaPorCodigo;
+    public javax.swing.JTextField jtfCodigoProducto;
+    public javax.swing.JTextField jtfCostoUnitario;
+    public javax.swing.JTextField jtfDescripcion;
+    public javax.swing.JTextField jtfExistenciasIniciales;
+    public javax.swing.JTextField jtfGrado;
     // End of variables declaration//GEN-END:variables
 }
